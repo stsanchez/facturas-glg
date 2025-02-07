134ac80ead94d2e058e904c2e1a6da732faf5039
Stefano Sanchez <stefanosanchez8@gmail.com>
2025-02-07 15:29:19 -0300

    trabajo prod y desa

---
+++ dev.py
@@ -0,0 +1,5 @@
+# dev.py
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)  # debug=True para activar el modo de depuración
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from app import app  # Reemplaza 'app' con el nombre de tu archivo principal si es diferente
+
+if __name__ == "__main__":
+    # No uses app.run() aquí en producción
+    pass
